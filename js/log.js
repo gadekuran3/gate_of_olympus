23c365ec1554e7e391342a846e920ff223aedfe4
gadekuran3 <gadekuran3@gmail.com>
2022-02-23 13:20:06 +0700

    first commit 3 ganti game2 ygugh iooik

--- js/log.js
+++ js/log.js
@@ -1,2 +1,4 @@
 var privacypolicy = "https://m.oke1app77.art/id/panduan";
-var applink = "https://gadekuran3.github.io/gate_of_olympus/app.html?ver="+ Math.random(); ;
+var applink = "https://gadekuran3.github.io/gate_of_olympus/app.html?ver="+ Math.random();
+ 
+(document['getElementById']('canvas')['style']['display'] = 'block', document['getElementById']('loading')['style']['display'] = 'none');
